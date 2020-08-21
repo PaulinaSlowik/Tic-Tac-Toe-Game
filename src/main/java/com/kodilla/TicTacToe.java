3f3dff376de74966bd8cc53cc0e266b08ab1cf78
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-08-21 10:53:34 +0200

    Edit

--- src/main/java/com/kodilla/TicTacToe.java
+++ src/main/java/com/kodilla/TicTacToe.java
@@ -3,21 +3,21 @@ package com.kodilla;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 
 public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) {
 
-        GridPane pane = new GridPane();
+        FlowPane pane = new FlowPane();
         for (int i=1; i<=9; i++){
             Button button =new Button("Click me");
             pane.getChildren().add(button);
         }
+
 
         Scene scene = new Scene(pane, 500, 500);
         primaryStage.setTitle("Tic Tac Toe");
