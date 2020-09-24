2f09ad1aef811178ef9fb1b323270b735fe4a87c
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-09-24 10:48:47 +0200

    final version with spring in test

---
+++ src/test/java/com/kodilla/GameApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+    void contextLoads() {
+    }
+}
